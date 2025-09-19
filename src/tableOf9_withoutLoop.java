7f66e0a79f4ff7cf4a9e2ecfd5526fec782eabd6
satyamsaraswat <59837310+satyamsaraswat@users.noreply.github.com>
2025-09-19 08:11:12 +0530

    Initial commit- TableOf9_without loop

---
+++ src/tableOf9_withoutLoop.java
@@ -0,0 +1,20 @@
+public class tableOf9_withoutLoop {
+    public static void main(String[] args) {
+        int number = 9;
+        int mult = 1;
+        int count = 1;
+
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+        System.out.printf("%d * %d = %d%n", number, count++, (mult++*number));
+
+
+    }
+}
